66d47bb1782c99151796149fae07007d5f39926e
rahul-pith1331 <rahul.rockersinfo@gmail.com>
2025-10-04 12:56:53 +0530

    chore: enforce LF endings with .gitattributes and .editorconfig

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+﻿import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
